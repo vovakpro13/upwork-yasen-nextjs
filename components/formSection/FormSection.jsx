34d578af351e0bfa88078d2408a150dcd69bcca3
Denys Botsyun <denwa62@gmail.com>
2023-08-04 15:11:00 +0300

    added magnet link animations for buttons

--- components/formSection/FormSection.jsx
+++ components/formSection/FormSection.jsx
@@ -1,184 +1,254 @@
-"use client"
+"use client";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import './style.css'
+import "./style.css";
 import Link from "next/link";
+import AnimatedButton from "../AnimatedButton";
 
 function FormSection() {
+  useEffect(() => {
+    const forms = document.querySelectorAll(".needs-validation");
+    const result = document.getElementById("result");
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach(function (form) {
+      form.addEventListener(
+        "submit",
+        function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
 
-    useEffect(() => {
-        const forms = document.querySelectorAll(".needs-validation");
-        const result = document.getElementById("result");
-        // Loop over them and prevent submission
-        Array.prototype.slice.call(forms).forEach(function (form) {
-            form.addEventListener(
-                "submit",
-                function (event) {
-                    if (!form.checkValidity()) {
-                        event.preventDefault();
-                        event.stopPropagation();
+            form.querySelectorAll(":invalid")[0].focus();
+          } else {
+            /*
+             * Form Submission using fetch()
+             */
 
-                        form.querySelectorAll(":invalid")[0].focus();
-                    } else {
-                        /*
-                         * Form Submission using fetch()
-                         */
+            const formData = new FormData(form);
+            event.preventDefault();
+            event.stopPropagation();
+            const object = {};
+            formData.forEach((value, key) => {
+              object[key] = value;
+            });
+            const json = JSON.stringify(object);
+            result.innerHTML = "Please wait...";
 
-                        const formData = new FormData(form);
-                        event.preventDefault();
-                        event.stopPropagation();
-                        const object = {};
-                        formData.forEach((value, key) => {
-                            object[key] = value;
-                        });
-                        const json = JSON.stringify(object);
-                        result.innerHTML = "Please wait...";
+            fetch("https://api.web3forms.com/submit", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+              },
+              body: json,
+            })
+              .then(async (response) => {
+                let json = await response.json();
+                if (response.status === 200) {
+                  result.innerHTML = json.message;
+                  result.classList.remove("text-gray-500");
+                  result.classList.add("text-green-500");
+                } else {
+                  console.log(response);
+                  result.innerHTML = json.message;
+                  result.classList.remove("text-gray-500");
+                  result.classList.add("text-red-500");
+                }
+              })
+              .catch((error) => {
+                console.log(error);
+                result.innerHTML = "Something went wrong!";
+              })
+              .then(function () {
+                form.reset();
+                form.classList.remove("was-validated");
+                setTimeout(() => {
+                  result.style.display = "none";
+                }, 5000);
+              });
+          }
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  }, []);
 
-                        fetch("https://api.web3forms.com/submit", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                                Accept: "application/json"
-                            },
-                            body: json
-                        })
-                            .then(async (response) => {
-                                let json = await response.json();
-                                if (response.status === 200) {
-                                    result.innerHTML = json.message;
-                                    result.classList.remove("text-gray-500");
-                                    result.classList.add("text-green-500");
-                                } else {
-                                    console.log(response);
-                                    result.innerHTML = json.message;
-                                    result.classList.remove("text-gray-500");
-                                    result.classList.add("text-red-500");
-                                }
-                            })
-                            .catch((error) => {
-                                console.log(error);
-                                result.innerHTML = "Something went wrong!";
-                            })
-                            .then(function () {
-                                form.reset();
-                                form.classList.remove("was-validated");
-                                setTimeout(() => {
-                                    result.style.display = "none";
-                                }, 5000);
-                            });
-                    }
-                    form.classList.add("was-validated");
-                },
-                false
-            );
-        });
-    }, [])
-
-    return (
-        <section id="form-section" className="form-section px-[16px] lg:px-32 lg:pt-[143px] lg:pb-[160px]">
-            <div className="form-container">
-                <div className="left-section relative xl:pb-[145px]">
-                    <h2 className="form-title text-gold font-donpoligrafbum"><span
-                        className="font-montserrat form-title-do gold-underline">СВЬРЖЕТЕ СЕ </span> с нас -</h2>
-                    <div className="form-caption font-montserrat text-[56px] xl:text-[56px] xl:max-w-[600px] text-right">{" "}
-                        за безплатен видео одит на Вашият уебсайт или идея
-                    </div>
-                    <div
-                        className="form-questions relative font-montserrat xl:text-[36px] text-gold xl:max-w-[366px] xl:mt-[33px] xl:ml-[143px] xl:leading-[43px]">
-                        Ще Ви отговорим
-                         <Link href="/" className="gold-underline">в рамките на деня.</Link>
-                        <Image
-                            src="/left-line-form.svg"
-                            alt="rectangle"
-                            width={133}
-                            height={10}
-                            className="left-line-form absolute top-1/2 left-[-91%]"
-                        />
-                    </div>
-                    <Image
-                        src="/blackdots.svg"
-                        alt="rectangle"
-                        width={94}
-                        height={81}
-                        className="absolute xl:right-[37%] bottom-0 form-dots-pic"
-                    />
-                </div>
-                <div className="right-section relative">
-                    <Image
-                        src="/form-line-right.svg"
-                        alt="rectangle"
-                        width={133}
-                        height={10}
-                        className="left-line-form absolute top-[18px] right-[-133px] xl:inline hidden"
-                    />
-                    <form action="https://api.web3forms.com/submit" method="POST" id="form"
-                          className="needs-validation" noValidate>
-                        <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE"/>
-                        <input type="hidden" name="subject" value="New Submission from Web3Forms"/>
-                        <div className="mb-[41px] relative">
-                            <input type="text" name="name" id="first_name" placeholder="Име" required
-                                   className="w-full px-3 py-2 placeholder-gray-300 border-b-[1px] border-[#1E1E1E] focus:outline-none form-item font-montserrat"/>
-                            <div className="empty-feedback invalid-feedback text-red-400 text-sm mt-1 absolute">
-                                Please provide your name.
-                            </div>
-                        </div>
-                        <div className="mb-[41px] relative">
-                            <input type="text" name="phone" id="phone" placeholder="Телефон"
-                                   required
-                                   className="w-full px-3 py-2 placeholder-gray-300 border-b-[1px] border-[#1E1E1E] focus:outline-none form-item font-montserrat"/>
-
-                            <div className="empty-feedback invalid-feedback text-red-400 text-sm mt-1 absolute">
-                                Please provide your phone number.
-                            </div>
-                        </div>
-                        <div className="mb-[41px] relative">
-                            <input type="email" name="email" id="email" placeholder="Имейл"
-                                   required
-                                   className="w-full px-3 py-2 placeholder-gray-300 border-b-[1px] border-[#1E1E1E] focus:outline-none form-item font-montserrat"/>
-                            <div className="empty-feedback text-red-400 text-sm mt-1 absolute">
-                                Please provide your email address.
-                            </div>
-                            <div className="invalid-feedback text-red-400 text-sm mt-1 absolute">
-                                Please provide a valid email address.
-                            </div>
-                        </div>
-
-                        <div className="flex gap-10 xl:mb-[95px] space-x-4 pt-[10px] form-radio-wrap">
-                            <div className="">
-                                <input className="item-radio" type="radio" id="test1" name="radio-group" checked />
-                                <label className="font-montserrat text-[18px]" htmlFor="test1">{'I don\'t have a website'}</label>
-                            </div>
-                            <div className="xl:ml-[41px] form-radio">
-                                <input className="item-radio"  type="radio" id="test2" name="radio-group" />
-                                <label className="font-montserrat text-[18px]" htmlFor="test2">I have a website and want to order a review</label>
-                            </div>
-                        </div>
-                        <div className="flex gap-10 mb-6 justify-between form-send-agree-wrap">
-                            <div className="flex items-center">
-                                <input className="item-checkbox"  type="checkbox" id="one" />
-                                <label className="label-checkbox font-montserrat" htmlFor="one">
-                                    <span className="relative"></span> I agree to the rules of personal data processing
-                                </label>
-                            </div>
-                            <button type="submit"
-                                    className="w-full relative px-3 font-montserrat text-[26px] text-[#1E1E1E] focus:outline-none btn-send">
-                                Изпрати
-                                <Image
-                                    src="/footer/icon-send.svg"
-                                    alt="rectangle"
-                                    width={24}
-                                    height={24}
-                                    className="absolute top-0 btn-send-icon"
-                                />
-                            </button>
-                        </div>
-                        <p className="text-base text-center text-gray-400" id="result"></p>
-                    </form>
-                </div>
+  return (
+    <section
+      id="form-section"
+      className="form-section px-[16px] lg:px-32 lg:pt-[143px] lg:pb-[160px]"
+    >
+      <div className="form-container">
+        <div className="left-section relative xl:pb-[145px]">
+          <h2 className="form-title text-gold font-donpoligrafbum">
+            <span className="font-montserrat form-title-do gold-underline">
+              СВЬРЖЕТЕ СЕ{" "}
+            </span>{" "}
+            с нас -
+          </h2>
+          <div className="form-caption font-montserrat text-[56px] xl:text-[56px] xl:max-w-[600px] text-right">
+            {" "}
+            за безплатен видео одит на Вашият уебсайт или идея
+          </div>
+          <div className="form-questions relative font-montserrat xl:text-[36px] text-gold xl:max-w-[366px] xl:mt-[33px] xl:ml-[143px] xl:leading-[43px]">
+            Ще Ви отговорим
+            <Link href="/" className="gold-underline">
+              в рамките на деня.
+            </Link>
+            <Image
+              src="/left-line-form.svg"
+              alt="rectangle"
+              width={133}
+              height={10}
+              className="left-line-form absolute top-1/2 left-[-91%]"
+            />
+          </div>
+          <Image
+            src="/blackdots.svg"
+            alt="rectangle"
+            width={94}
+            height={81}
+            className="absolute xl:right-[37%] bottom-0 form-dots-pic"
+          />
+        </div>
+        <div className="right-section relative">
+          <Image
+            src="/form-line-right.svg"
+            alt="rectangle"
+            width={133}
+            height={10}
+            className="left-line-form absolute top-[18px] right-[-133px] xl:inline hidden"
+          />
+          <form
+            action="https://api.web3forms.com/submit"
+            method="POST"
+            id="form"
+            className="needs-validation"
+            noValidate
+          >
+            <input
+              type="hidden"
+              name="access_key"
+              value="YOUR_ACCESS_KEY_HERE"
+            />
+            <input
+              type="hidden"
+              name="subject"
+              value="New Submission from Web3Forms"
+            />
+            <div className="mb-[41px] relative">
+              <input
+                type="text"
+                name="name"
+                id="first_name"
+                placeholder="Име"
+                required
+                className="w-full px-3 py-2 placeholder-gray-300 border-b-[1px] border-[#1E1E1E] focus:outline-none form-item font-montserrat"
+              />
+              <div className="empty-feedback invalid-feedback text-red-400 text-sm mt-1 absolute">
+                Please provide your name.
+              </div>
             </div>
-        </section>
-);
+            <div className="mb-[41px] relative">
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                placeholder="Телефон"
+                required
+                className="w-full px-3 py-2 placeholder-gray-300 border-b-[1px] border-[#1E1E1E] focus:outline-none form-item font-montserrat"
+              />
+
+              <div className="empty-feedback invalid-feedback text-red-400 text-sm mt-1 absolute">
+                Please provide your phone number.
+              </div>
+            </div>
+            <div className="mb-[41px] relative">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Имейл"
+                required
+                className="w-full px-3 py-2 placeholder-gray-300 border-b-[1px] border-[#1E1E1E] focus:outline-none form-item font-montserrat"
+              />
+              <div className="empty-feedback text-red-400 text-sm mt-1 absolute">
+                Please provide your email address.
+              </div>
+              <div className="invalid-feedback text-red-400 text-sm mt-1 absolute">
+                Please provide a valid email address.
+              </div>
+            </div>
+
+            <div className="flex gap-10 xl:mb-[95px] space-x-4 pt-[10px] form-radio-wrap">
+              <div className="">
+                <input
+                  className="item-radio"
+                  type="radio"
+                  id="test1"
+                  name="radio-group"
+                  checked
+                />
+                <label className="font-montserrat text-[18px]" htmlFor="test1">
+                  {"I don't have a website"}
+                </label>
+              </div>
+              <div className="xl:ml-[41px] form-radio">
+                <input
+                  className="item-radio"
+                  type="radio"
+                  id="test2"
+                  name="radio-group"
+                />
+                <label className="font-montserrat text-[18px]" htmlFor="test2">
+                  I have a website and want to order a review
+                </label>
+              </div>
+            </div>
+            <div className="flex gap-10 mb-6 justify-between form-send-agree-wrap">
+              <div className="flex items-center">
+                <input className="item-checkbox" type="checkbox" id="one" />
+                <label className="label-checkbox font-montserrat" htmlFor="one">
+                  <span className="relative"></span> I agree to the rules of
+                  personal data processing
+                </label>
+              </div>
+              <AnimatedButton
+                type="submit"
+                classname="w-full relative px-3 font-montserrat text-[26px] text-[#1E1E1E] focus:outline-none btn-send"
+              >
+                Изпрати
+                <Image
+                  src="/footer/icon-send.svg"
+                  alt="rectangle"
+                  width={24}
+                  height={24}
+                  className="absolute top-0 btn-send-icon"
+                />
+              </AnimatedButton>
+            </div>
+            <p className="text-base text-center text-gray-400" id="result"></p>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default FormSection;
+
+{/* <button
+type="submit"
+className="w-full relative px-3 font-montserrat text-[26px] text-[#1E1E1E] focus:outline-none btn-send"
+>
+Изпрати
+<Image
+  src="/footer/icon-send.svg"
+  alt="rectangle"
+  width={24}
+  height={24}
+  className="absolute top-0 btn-send-icon"
+/>
+</button> */}
--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,34 +1,35 @@
 "use client";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import AnimatedLink from "../AnimatedLink";
 import Image from "next/image";
 import Link from "next/link";
-import './navBar.css'
+import "./navBar.css";
 import BurgerNavbar from "@/components/layout/BurgerNavBar";
 const Navbar = () => {
-  const [position, setPosition] = useState(0)
-  const [visible, setVisible] = useState(true)
-  useEffect(()=> {
-    if (typeof window !== 'undefined') {
+  const [position, setPosition] = useState(0);
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
       const handleScroll = () => {
-
         let moving = window.scrollY;
 
         setVisible(position > moving);
-        setPosition(moving)
+        setPosition(moving);
       };
       window.addEventListener("scroll", handleScroll);
-      return(() => {
+      return () => {
         window.removeEventListener("scroll", handleScroll);
-      })
+      };
     }
-
-  })
+  });
 
   const cls = visible ? "visible-nav" : "hidden-nav";
   return (
-    <div className={`${cls} w-full fixed z-[101] wrapper-nav-global md:h-[128px] h-[80px]`}>
+    <div
+      className={`${cls} w-full fixed z-[101] wrapper-nav-global md:h-[128px] h-[80px]`}
+    >
       <nav className="navbar w-full flex justify-between bg-transparent z-50 fixed overflow-hidden lg:px-32">
-        <div className="left-container left-container-custom flex items-center relative w-[43%] lg:w-[41%] border-b-[1px] border-b-primary relative z-10">
+        <div className="left-container left-container-custom flex items-center w-[43%] lg:w-[41%] border-b-[1px] border-b-primary relative z-10">
           <Image
             src="/Rectangle 15.svg"
             alt="rectangle"
@@ -37,12 +38,14 @@ const Navbar = () => {
             className="absolute -bottom-[5px] right-[23px] lg:right-28"
           />
 
-
-          <Link href="tel:+1234567890" className="tel hover-nav text-white font-montserrat font-semibold hidden lg:block top-[5px] relative">
+          <Link
+            href="tel:+1234567890"
+            className="tel hover-nav text-white font-montserrat font-semibold hidden lg:block top-[5px] relative"
+          >
             +1 234 56 78 90
           </Link>
         </div>
-        <div className="right-container right-container-header gap-2 flex justify-between relative w-[55%] lg:w-[58%] h-[80px] lg:h-[123px] border-b-[1px] border-b-primary relative z-10">
+        <div className="right-container right-container-header gap-2 flex justify-between w-[55%] lg:w-[58%] h-[80px] lg:h-[123px] border-b-[1px] border-b-primary relative z-10">
           <div className="logo-container w-1/2 h-14 lg:h-[82px] border-r border-l border-primary">
             <Image
               src="/logo-jmax.svg"
@@ -52,52 +55,63 @@ const Navbar = () => {
               className="m-auto lg:translate-y-1 translate-y-3 relative top-3 logo"
             />
           </div>
-          <Link href="tel:+1234567890" className="mob-tel text-white w-1/2 font-montserrat lg:ml-9 mt-7 lg:mt-5 lg:font-semibold text-[10px] sm:text-lg block lg:hidden text-right whitespace-nowrap">
+          <Link
+            href="tel:+1234567890"
+            className="mob-tel text-white w-1/2 font-montserrat lg:ml-9 mt-7 lg:mt-5 lg:font-semibold text-[10px] sm:text-lg block lg:hidden text-right whitespace-nowrap"
+          >
             +1 234 56 78 90
           </Link>
           <div className="buttons-container hidden lg:flex  justify-around items-center relative z-30">
-            <Link href="#" className="relative z-0 px-[13px] header-btn top-[4px]">
+            <AnimatedLink
+              classname="relative z-0 px-[13px] header-btn top-[4px]"
+              href="#"
+            >
               <hr className="nav-button h-24 -top-5 w-0 absolute z-0 border-primary border-t-[3px] rounded-sm " />
               <span className="header-btn__border absolute left-0 top-0">
                 <Image
-                    src="/call.svg"
-                    alt="button"
-                    width={24}
-                    height={24}
-                    className="absolute left-[39px] -top-[10px] icon"
+                  src="/call.svg"
+                  width={24}
+                  height={24}
+                  alt="button"
+                  className="absolute left-[39px] -top-[10px] icon"
                 />
                 <Image
-                    src="/btn-border.svg"
-                    alt="button"
-                    width={200}
-                    height={54}
-                    className="w-full"
+                  src="/btn-border.svg"
+                  alt="button"
+                  width={200}
+                  height={54}
+                  className="w-full"
                 />
               </span>
-              <span className="header-btn__text font-montserrat">КОНСУЛТАЦИЯ</span>
-
-            </Link>
-            <Link href="#form-section" className="relative z-0 px-[13px] header-btn top-[4px] -left-[4px] pr-0">
+              <span className="header-btn__text font-montserrat">
+                КОНСУЛТАЦИЯ
+              </span>
+            </AnimatedLink>
+            <AnimatedLink
+              classname="relative z-0 px-[13px] header-btn top-[4px]"
+              href="#form-section"
+            >
               <hr className="nav-button h-24 -top-5 w-0 absolute z-0 border-primary border-t-[3px] rounded-sm " />
               <span className="header-btn__border absolute left-0 top-0">
                 <Image
-                    src="/document.svg"
-                    alt="button"
-                    width={24}
-                    height={24}
-                    className="absolute left-[39px] -top-[10px] icon"
+                  src="/document.svg"
+                  width={24}
+                  height={24}
+                  alt="button"
+                  className="absolute left-[39px] -top-[10px] icon"
                 />
                 <Image
-                    src="/btn-border.svg"
-                    alt="button"
-                    width={200}
-                    height={54}
-                    className="w-full"
+                  src="/btn-border.svg"
+                  alt="button"
+                  width={200}
+                  height={54}
+                  className="w-full"
                 />
               </span>
-              <span className="header-btn__text font-montserrat">БЕЗПЛАТЕН ОДИТ</span>
-
-            </Link>
+              <span className="header-btn__text font-montserrat">
+                БЕЗПЛАТЕН ОДИТ
+              </span>
+            </AnimatedLink>
           </div>
           <Image
             src="/Rectangle 15.svg"
